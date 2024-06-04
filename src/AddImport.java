fd40114f37b3f4d218ebd6fced217cdde3b3d547
Aluno <Aluno@10.85.198.10>
2024-06-04 16:49:13 -0300

    Commit H no metodo

--- src/AddImport.java
+++ src/AddImport.java
@@ -4,7 +4,7 @@ import javax.swing.JLabel;
 public class AddImport {
 
     public static void main(String args[]) {
-        JLabel label = new JLabel("hello");
+        JLabel label = new JLabel("Hello");
 
     }
 
